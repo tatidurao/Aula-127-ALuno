db59564364fa7139cc1075f12248189779393084
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2023-07-12 13:47:30 -0300

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
-# URL dos Exoplanetas da NASA
+
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 
 # Webdriver
-browser = webdriver.Chrome("D:/Area de Trabalho/whitehatjr/Pro Versão 2 - Python/c127")
-browser.get(START_URL)
+#browser = webdriver.Chrome("D:/Area de Trabalho/whitehatjr/Pro Versão 2 - Python/c127")
+#browser.get(START_URL)
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get(START_URL)
+
 
 time.sleep(10)
+
 
 planets_data = []
 
