7043fa18a8d72e327a8f462ae1b456c5597eb62f
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2024-10-16 13:24:59 -0300

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
@@ -9,16 +7,9 @@ import pandas as pd
 
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 
-# Webdriver
-#browser = webdriver.Chrome("D:/Area de Trabalho/whitehatjr/Pro Versão 2 - Python/c127")
-#browser.get(START_URL)
-
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Edge()
 driver.get(START_URL)
-
-
-time.sleep(10)
-
+driver.maximize_window()
 
 planets_data = []
 
@@ -27,8 +18,8 @@ planets_data = []
 headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
 
 # Defina o dataframe do pandas
-planet_df_1 = pd.DataFrame(planets_data, columns=headers)
+
 
 # Converta para CSV
 #para adicionar a primeira coluna com os numeros de serie o atributo index com a eitqueta id
-planet_df_1.to_csv('scraped_data.csv',index=True, index_label="id")
+
